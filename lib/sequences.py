10d3c8664a2771dd5f40fb662453dfc870e2cc9d
Your Name <kkamadi567@gmail.com>
2023-11-24 22:12:02 +0300

    added

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence = []
+    a, b = 0, 1
+
+    for _ in range(length):
+        fib_sequence.append(a)
+        a, b = b, a + b
+
+    print(fib_sequence)
+
+
+print_fibonacci(10)
+print_fibonacci(0)
+print_fibonacci(1)
+print_fibonacci(2)
